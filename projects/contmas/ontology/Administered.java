72b9d2a5965ee8da7326d971c6c56d51365058ee
Hanno - Felix Wagner <hanno-felix.wagner@uni-due.de>
2010-03-03 22:05:52 +0000

    git-svn-id: svn://project.agent-hygrid.net/agentgui/trunk/AgentGUI@150 014e83ad-c670-0410-b1a0-9290c87bb784

---
+++ projects/contmas/ontology/Administered.java
@@ -0,0 +1,16 @@
+package contmas.ontology;
+
+/**
+ * Protege name: Administered
+ * 
+ * @author ontology bean generator
+ * @version 2010/03/3, 11:16:16
+ */
+public class Administered extends TransportOrderChainState{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID=897694671776289004L;
+
+}
