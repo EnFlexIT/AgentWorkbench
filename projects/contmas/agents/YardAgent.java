04027a96641b75eebd07b46937684defb1b34701
Hanno - Felix Wagner <hanno-felix.wagner@uni-due.de>
2010-03-11 22:27:47 +0000

    *inner agent changes *change in harbour structure: ship-crane-agv -> ship-apron-straddlecarrier-yard
    git-svn-id: svn://project.agent-hygrid.net/agentgui/trunk/AgentGUI@180 014e83ad-c670-0410-b1a0-9290c87bb784

---
+++ projects/contmas/agents/YardAgent.java
@@ -0,0 +1,66 @@
+/**
+ * @author Hanno - Felix Wagner, 11.03.2010
+ * Copyright 2010 Hanno - Felix Wagner
+ * 
+ * This file is part of ContMAS.
+ *
+ * ContMAS is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * ContMAS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with ContMAS.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+package contmas.agents;
+
+import jade.domain.FIPANames;
+import jade.lang.acl.MessageTemplate;
+import jade.proto.ContractNetResponder;
+import contmas.behaviours.receiveLoadOrders;
+import contmas.ontology.StaticContainerHolder;
+import contmas.ontology.StraddleCarrier;
+import contmas.ontology.Yard;
+import contmas.ontology.YardArea;
+
+/**
+ * @author Hanno - Felix Wagner
+ *
+ */
+public class YardAgent extends StaticContainerAgent implements TransportOrderHandler{
+
+	public YardAgent(){
+		this(new Yard());
+	}
+
+	/**
+	 * @param serviceType
+	 * @param ontologyRepresentation
+	 */
+	public YardAgent(Yard ontologyRepresentation){
+		super("container-storing",ontologyRepresentation);
+		targetAgentDFDescription=null;
+		targetAbstractDomain=null;
+	}
+
+	/* (non-Javadoc)
+	 * @see contmas.agents.TransportOrderHandler#handleTransportOrder()
+	 */
+	@Override
+	public void handleTransportOrder(){
+		MessageTemplate mt=ContractNetResponder.createMessageTemplate(FIPANames.InteractionProtocol.FIPA_CONTRACT_NET);
+		this.addBehaviour(new receiveLoadOrders(this,mt));
+	}
+	@Override
+	public void setup(){
+		super.setup();
+		this.handleTransportOrder();
+	}
+
+}
