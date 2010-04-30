1c1bf32d5b90dfef983f83648fd4d562b089eb44
Nils Loose <nils.loose@uni-due.de>
2010-04-30 07:45:46 +0000

    - Fehler behoben
    git-svn-id: svn://project.agent-hygrid.net/agentgui/trunk/AgentGUI@231 014e83ad-c670-0410-b1a0-9290c87bb784

--- src/mas/environment/EnvironmentControllerGUI.java
+++ src/mas/environment/EnvironmentControllerGUI.java
@@ -524,7 +524,7 @@ public class EnvironmentControllerGUI extends JSplitPane {
 			lblX1.setSize(lblX1.getPreferredSize());
 			lblX1.setLocation(new Point(65, 142));
 			lblUnit1 = new JLabel();
-			lblUnit1.setText(controller.getEnvironment().getScale().getUnit());
+//			lblUnit1.setText(controller.getEnvironment().getScale().getUnit());
 			lblUnit1.setSize(lblUnit1.getPreferredSize());
 			lblUnit1.setLocation(new Point(122, 142));
 			lblSize = new JLabel();
@@ -536,7 +536,7 @@ public class EnvironmentControllerGUI extends JSplitPane {
 			lblX2.setSize(lblX2.getPreferredSize());
 			lblX2.setLocation(new Point(63, 212));
 			lblUnit2 = new JLabel();
-			lblUnit2.setText(controller.getEnvironment().getScale().getUnit());
+//			lblUnit2.setText(controller.getEnvironment().getScale().getUnit());
 			lblUnit2.setSize(lblUnit2.getPreferredSize());
 			lblUnit2.setLocation(new Point(122, 212));
 			pnlObjectSettings.add(lblPos, null);
