3ad7988ada1941a977b8bb92949d5e74c47c76d6
Marvin Steinberg <marvin.steinberg@gmx.de>
2010-06-09 11:29:58 +0000

    GUI für Sim-Setup erzeugen - Teil 1
    git-svn-id: svn://project.agent-hygrid.net/agentgui/trunk/AgentGUI@311 014e83ad-c670-0410-b1a0-9290c87bb784

---
+++ src/sim/setup/gui/DynForm.java
@@ -0,0 +1,26 @@
+package sim.setup.gui;
+
+import javax.swing.JDialog;
+import javax.swing.JPanel;
+
+public class DynForm extends JPanel{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 7942028680794127910L;
+
+	public static void main(String[] args) {
+		
+		JDialog jd = new JDialog();
+		jd.add(new DynForm(args));
+		
+	}
+	
+	public DynForm(String[] startObjectClass) {
+		
+		
+		
+	}
+	
+}
