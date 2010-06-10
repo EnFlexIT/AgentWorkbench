3bdd981e54553ec98d77a725b5ce2f1caf92c09d
Hanno - Felix Wagner <hanno-felix.wagner@uni-due.de>
2010-06-10 03:48:46 +0000

    changed  BayMapOptimizer's emptyBlockAddress determination to get rid of containers-sticking-out-of-baymap-bug
    git-svn-id: svn://project.agent-hygrid.net/agentgui/trunk/AgentGUI@315 014e83ad-c670-0410-b1a0-9290c87bb784

--- projects/contmas/agents/BayMapOptimisationAgent.java
+++ projects/contmas/agents/BayMapOptimisationAgent.java
@@ -18,7 +18,7 @@ public class BayMapOptimisationAgent extends ContainerAgent{
 
 	private static final long serialVersionUID= -2081698387513185474L;
 	private Random RandomGenerator=new Random();
-
+	
 	public BayMapOptimisationAgent(){
 		super("optimizing-bay-maps");
 	}
@@ -31,29 +31,39 @@ public class BayMapOptimisationAgent extends ContainerAgent{
 	}
 
 	public BlockAddress getEmptyBlockAddress(BayMap loadBay,List allContainers){
-		Integer tries=0;
-		Integer maxTries=getBayMapCapacity(loadBay);
-		Integer x, y, z;
-		while(tries < maxTries){
-			x=RandomGenerator.nextInt(loadBay.getX_dimension());
-			y=RandomGenerator.nextInt(loadBay.getY_dimension());
-			BlockAddress upmostContainer=this.getUpmostOccupiedBlockAddress(x,y,loadBay,allContainers);
-			z=0;
-			if(upmostContainer != null){
-				if(upmostContainer.getZ_dimension() < loadBay.getZ_dimension()){ //there is room on top
-					z=upmostContainer.getZ_dimension() + 1;
-				}else{
-					tries++;
-					continue;
-				}
-			}
-			BlockAddress emptyBA=new BlockAddress();
-			emptyBA.setX_dimension(x);
-			emptyBA.setY_dimension(y);
-			emptyBA.setZ_dimension(z);
+		BlockAddress emptyBA=null;
+		
+		if(allContainers.size()==getBayMapCapacity(loadBay)){ //bay map is already full
 			return emptyBA;
 		}
-		return null;
+
+		TransportOrderChainState[][][] bayMapArray=getBayMapArray(loadBay, allContainers);
+		
+		Integer x, y, z=0;
+		while(emptyBA==null){
+			x=RandomGenerator.nextInt(loadBay.getX_dimension());
+			y=RandomGenerator.nextInt(loadBay.getY_dimension());
+			BlockAddress upmostContainer;
+			try {
+				upmostContainer = this.getUpmostOccupiedBlockAddress(x,y,bayMapArray);
+			} catch (BlockedBecausePlanningException e) {
+				echoStatus("Stack at current coordinate blocked because of pending plans, skipping.",ContainerAgent.LOGGING_INFORM); //leads to containers hanging in the air
+				continue;
+			}
+	
+			if(upmostContainer == null || (upmostContainer.getZ_dimension() < loadBay.getZ_dimension()-1)){ //there is room on top
+				if(upmostContainer != null){
+					z=upmostContainer.getZ_dimension() + 1;
+				}
+				
+				emptyBA=new BlockAddress();
+				emptyBA.setX_dimension(x);
+				emptyBA.setY_dimension(y);
+				emptyBA.setZ_dimension(z);
+
+			}
+		}
+		return emptyBA;
 	}
 
 	public BlockAddress getEmptyBlockAddress(BayMap loadBay,TransportOrderChain subject,List allContainers){
@@ -67,10 +77,31 @@ public class BayMapOptimisationAgent extends ContainerAgent{
 		return baySize;
 	}
 
+	public BlockAddress getUpmostOccupiedBlockAddress(Integer x,Integer y,TransportOrderChainState[][][] bayMapArray) throws BlockedBecausePlanningException{
+		BlockAddress upmostOcc=null;
+		TransportOrderChainState[] stack=bayMapArray[x][y];
+		
+		for (Integer i = 0; i < stack.length; i++) {
+			if(stack[i]!=null){ //there is a container at this position
+				if(stack[i] instanceof Holding){
+					upmostOcc=stack[i].getAt_address(); //memorize it
+				} else {
+					throw new BlockedBecausePlanningException();
+				}
+			} else { //no container at this position
+				if(upmostOcc!=null){ //already found some container, that one is the upmost
+					break;
+				}
+			}
+		}
+		return upmostOcc;
+	}
+	
 	public BlockAddress getUpmostOccupiedBlockAddress(Integer x,Integer y,BayMap loadBay,List allContainers){
+		
 		BlockAddress upmostContainer=null;
 		Iterator allConts=allContainers.iterator();
-		while(allConts.hasNext()){ //alle geladenen Container überprüfen
+		while(allConts.hasNext()){ //check all containers
 			TransportOrderChainState curState=((TOCHasState) allConts.next()).getState();
 			if(curState instanceof Holding){
 				BlockAddress curContainer=(BlockAddress) curState.getAt_address();
@@ -85,5 +116,22 @@ public class BayMapOptimisationAgent extends ContainerAgent{
 			}
 		} //end while
 		return upmostContainer;
+	}
+	
+	public TransportOrderChainState[][][] getBayMapArray(BayMap loadBay,List allContainers){
+		TransportOrderChainState[][][] bayMapArray=new TransportOrderChainState[loadBay.getX_dimension()][loadBay.getY_dimension()][loadBay.getZ_dimension()];
+		
+		Iterator allConts=allContainers.iterator();
+		while(allConts.hasNext()){
+			TransportOrderChainState curState=((TOCHasState) allConts.next()).getState();
+			BlockAddress curContainer=(BlockAddress) curState.getAt_address();
+			bayMapArray[curContainer.getX_dimension()][curContainer.getY_dimension()][curContainer.getZ_dimension()]=curState;
+		}
+		
+		return bayMapArray;
+	}
+	
+	private class BlockedBecausePlanningException extends Exception{
+		
 	}
 }
