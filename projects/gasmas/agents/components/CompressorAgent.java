ff29d16eb449d6185ef83518d180a57ff99c8f93
Christian Derksen <christian.derksen@uni-due.de>
2011-11-25 15:24:23 +0000

    Compressor Agent - example for accessing environment model.
    git-svn-id: svn://project.agent-hygrid.net/agentgui/trunk/AgentGUI@968 014e83ad-c670-0410-b1a0-9290c87bb784

--- projects/gasmas/agents/components/CompressorAgent.java
+++ projects/gasmas/agents/components/CompressorAgent.java
@@ -1,7 +1,40 @@
 package gasmas.agents.components;
 
+import jade.core.ServiceException;
+import agentgui.simulationService.SimulationService;
+import agentgui.simulationService.SimulationServiceHelper;
 import agentgui.simulationService.agents.SimulationAgent;
+import agentgui.simulationService.environment.EnvironmentModel;
 
 public class CompressorAgent extends SimulationAgent {
 
+	private static final long serialVersionUID = 2792727750579482552L;
+
+	SimulationServiceHelper simHelper = null;
+	
+	@Override
+	protected void setup() {
+		super.setup();
+
+		while(this.myEnvironmentModel==null) {
+			
+			if (simHelper==null) {
+				try {
+					simHelper = (SimulationServiceHelper) getHelper(SimulationService.NAME);
+					EnvironmentModel envModel =  simHelper.getEnvironmentModel();
+					if (envModel!=null) {
+						this.myEnvironmentModel = envModel;
+						break;
+					}
+				} catch (ServiceException e) {
+					e.printStackTrace();
+				}
+			}
+
+			
+			
+		}
+		
+	}
+	
 }
