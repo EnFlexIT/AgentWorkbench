e7b7c64be80e27e8e8d8f94030edcedc43496cbb
Benjamin Schwartz <benjamin.schwartz@stud.uni-due.de>
2012-08-02 11:33:03 +0000

    Improvments of a simplification algo
    git-svn-id: svn://project.agent-hygrid.net/agentgui/trunk/AgentGUI@1291 014e83ad-c670-0410-b1a0-9290c87bb784

---
+++ projects/gasmas/agents/components/ClusterNetworkComponentAgent.java
@@ -0,0 +1,11 @@
+package gasmas.agents.components;
+
+public class ClusterNetworkComponentAgent extends GenericNetworkAgent {
+
+	private static final long serialVersionUID = 5755894155609484866L;
+
+	@Override
+	protected void setup() {
+		super.setup();
+	}
+}
