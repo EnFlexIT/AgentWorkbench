d30219ca217bc4c46c0cd83a436b3d6efbe1fe3c
Nils Loose <nils.loose@uni-due.de>
2012-12-20 20:01:10 +0000

    Erste Ladung strömungstechnisches Herumgeformel
    git-svn-id: svn://project.agent-hygrid.net/agentgui/trunk/AgentGUI@1378 014e83ad-c670-0410-b1a0-9290c87bb784

---
+++ projects/gasmas/physics/pipes/FluidVelocity.java
@@ -0,0 +1,103 @@
+package gasmas.physics.pipes;
+
+import agentgui.math.calculation.CalcConstant;
+import agentgui.math.calculation.CalcExeption;
+import agentgui.math.calculation.CalcExpression;
+import agentgui.math.calculation.CalcParameterNotSetException;
+
+/**
+ * Calculates the fluid velocity using Cerbe formula 4.2.2
+ * @author Nils
+ *
+ */
+public class FluidVelocity extends CalcExpression {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 896839078700630853L;
+	
+	// Parameters
+	private CalcExpression diameter;
+	private CalcExpression volumeFlow;
+	private CalcExpression normPreasure;
+	private CalcExpression realPreasure;
+	private CalcExpression normTemperature;
+	private CalcExpression realTemperature;
+	
+	public FluidVelocity(CalcExpression diameter, CalcExpression volumeStream, CalcExpression realPreasure, CalcExpression realTemperature){
+		this.diameter = diameter;
+		this.volumeFlow = volumeStream;
+		this.realPreasure = realPreasure;
+		this.realTemperature = realTemperature;
+		this.normTemperature = new CalcConstant(273);		// Norm temperature 273 K
+		this.normPreasure = new CalcConstant(1.01325); 	// Norm preasure 1.01325 bar
+	}
+	
+	/**
+	 * @return the diameter
+	 */
+	public CalcExpression getDiameter() {
+		return diameter;
+	}
+
+	/**
+	 * @param diameter the diameter to set
+	 */
+	public void setDiameter(CalcExpression diameter) {
+		this.diameter = diameter;
+	}
+
+	/**
+	 * @return the volumeStream
+	 */
+	public CalcExpression getVolumeFlow() {
+		return volumeFlow;
+	}
+
+	/**
+	 * @param volumeFlow the volumeStream to set
+	 */
+	public void setVolumeFlow(CalcExpression volumeFlow) {
+		this.volumeFlow = volumeFlow;
+	}
+
+	/**
+	 * @return the realPreasure
+	 */
+	public CalcExpression getRealPreasure() {
+		return realPreasure;
+	}
+
+	/**
+	 * @param realPreasure the realPreasure to set
+	 */
+	public void setRealPreasure(CalcExpression realPreasure) {
+		this.realPreasure = realPreasure;
+	}
+
+	/**
+	 * @return the realTemperature
+	 */
+	public CalcExpression getRealTemperature() {
+		return realTemperature;
+	}
+
+	/**
+	 * @param realTemperature the realTemperature to set
+	 */
+	public void setRealTemperature(CalcExpression realTemperature) {
+		this.realTemperature = realTemperature;
+	}
+
+	@Override
+	public double getValue() throws CalcExeption {
+		if( volumeFlow == null  || realPreasure == null || realTemperature == null){
+			throw new CalcParameterNotSetException();
+		}else{
+			double circleArea = Math.pow(diameter.getValue(),2) * Math.PI / 4;
+			return (volumeFlow.getValue() * normPreasure.getValue() * realTemperature.getValue())/ (circleArea * realPreasure.getValue() * normTemperature.getValue() * 3600);
+		}
+	}
+
+}
---
+++ projects/gasmas/physics/pipes/PipePreasureLoss.java
@@ -0,0 +1,208 @@
+package gasmas.physics.pipes;
+
+import agentgui.math.calculation.CalcExeption;
+import agentgui.math.calculation.CalcExpression;
+import agentgui.math.calculation.CalcParameterNotSetException;
+/**
+ * Calculates the preasure loss for a pipe 
+ * @author Nils
+ *
+ */
+public class PipePreasureLoss extends CalcExpression{
+
+	/**
+	 * Generated serialVersionUID
+	 */
+	private static final long serialVersionUID = 9077387906331047779L;
+	
+	// Parameters
+	private CalcExpression initialPreasure;
+	private CalcExpression pipeFrictionCoefficient;
+	private CalcExpression pipeLength;
+	private CalcExpression pipeDiameter;
+	private CalcExpression realGasDensity;
+	private CalcExpression fluidVelocity;
+	private CalcExpression compressibilityNumber;
+	
+	/**
+	 * @return All parameters set?
+	 */
+	private boolean checkParams(){
+		if(
+				initialPreasure == null
+				|| pipeFrictionCoefficient == null
+				|| pipeLength == null
+				|| pipeDiameter == null
+				|| realGasDensity == null
+				|| fluidVelocity == null
+				|| compressibilityNumber == null
+			){
+			return false;
+		}
+		return true;
+	}
+	
+	
+
+
+	/**
+	 * @return the initialPreasure
+	 */
+	public CalcExpression getInitialPreasure() {
+		return initialPreasure;
+	}
+
+
+
+
+	/**
+	 * @param initialPreasure the initialPreasure to set
+	 */
+	public void setInitialPreasure(CalcExpression initialPreasure) {
+		this.initialPreasure = initialPreasure;
+	}
+
+
+
+
+	/**
+	 * @return the pipeFrictionCoefficient
+	 */
+	public CalcExpression getPipeFrictionCoefficient() {
+		return pipeFrictionCoefficient;
+	}
+
+
+
+
+	/**
+	 * @param pipeFrictionCoefficient the pipeFrictionCoefficient to set
+	 */
+	public void setPipeFrictionCoefficient(CalcExpression pipeFrictionCoefficient) {
+		this.pipeFrictionCoefficient = pipeFrictionCoefficient;
+	}
+
+
+
+
+	/**
+	 * @return the pipeLength
+	 */
+	public CalcExpression getPipeLength() {
+		return pipeLength;
+	}
+
+
+
+
+	/**
+	 * @param pipeLength the pipeLength to set
+	 */
+	public void setPipeLength(CalcExpression pipeLength) {
+		this.pipeLength = pipeLength;
+	}
+
+
+
+
+	/**
+	 * @return the pipeDiameter
+	 */
+	public CalcExpression getPipeDiameter() {
+		return pipeDiameter;
+	}
+
+
+
+
+	/**
+	 * @param pipeDiameter the pipeDiameter to set
+	 */
+	public void setPipeDiameter(CalcExpression pipeDiameter) {
+		this.pipeDiameter = pipeDiameter;
+	}
+
+
+
+
+	/**
+	 * @return the realGasDensity
+	 */
+	public CalcExpression getRealGasDensity() {
+		return realGasDensity;
+	}
+
+
+
+
+	/**
+	 * @param realGasDensity the realGasDensity to set
+	 */
+	public void setRealGasDensity(CalcExpression realGasDensity) {
+		this.realGasDensity = realGasDensity;
+	}
+
+
+
+
+	/**
+	 * @return the fluidVelocity
+	 */
+	public CalcExpression getFluidVelocity() {
+		return fluidVelocity;
+	}
+
+
+
+
+	/**
+	 * @param fluidVelocity the fluidVelocity to set
+	 */
+	public void setFluidVelocity(CalcExpression fluidVelocity) {
+		this.fluidVelocity = fluidVelocity;
+	}
+
+
+
+
+	/**
+	 * @return the compressibilityNumber
+	 */
+	public CalcExpression getCompressibilityNumber() {
+		return compressibilityNumber;
+	}
+
+
+
+
+	/**
+	 * @param compressibilityNumber the compressibilityNumber to set
+	 */
+	public void setCompressibilityNumber(CalcExpression compressibilityNumber) {
+		this.compressibilityNumber = compressibilityNumber;
+	}
+
+
+
+
+	@Override
+	public double getValue() throws CalcExeption {
+		if(checkParams()){
+			double lambda = pipeFrictionCoefficient.getValue();
+			double l = pipeLength.getValue();
+			double d = pipeDiameter.getValue();
+			double ro = realGasDensity.getValue();
+			double p = initialPreasure.getValue();
+			double w = fluidVelocity.getValue();
+			double k = compressibilityNumber.getValue();
+			
+			double radicand = 1 - lambda * ( l / d ) * ( ro / p ) * w * k;
+			
+			return p * ( 1 - Math.pow(radicand, 0.5));
+		}else{
+			throw new CalcParameterNotSetException();
+		}
+	}
+	
+}
+
