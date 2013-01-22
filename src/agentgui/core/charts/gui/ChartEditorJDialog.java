0babfffd16cbd26ebd8c6cc90e60e29fcfd14eb6
Christian Derksen <christian.derksen@uni-due.de>
2013-01-22 20:40:36 +0000

    Revised Observable user of DynForm
    git-svn-id: svn://project.agent-hygrid.net/agentgui/trunk/AgentGUI@1426 014e83ad-c670-0410-b1a0-9290c87bb784

--- src/agentgui/core/charts/gui/ChartEditorJDialog.java
+++ src/agentgui/core/charts/gui/ChartEditorJDialog.java
@@ -4,7 +4,6 @@ import java.awt.BorderLayout;
 import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.image.BufferedImage;
 
 import javax.swing.JButton;
 import javax.swing.JDialog;
