8a4f3c65c5e251591a2d69f61bac3b39de65ad32
Christian Derksen <christian.derksen@uni-due.de>
2014-02-18 00:02:54 +0000

    Prepared version 1.01 of Agent.GUI
    git-svn-id: svn://project.agent-hygrid.net/agentgui/trunk/AgentGUI@1703 014e83ad-c670-0410-b1a0-9290c87bb784

--- src/agentgui/core/update/AgentGuiUpdaterTest.java
+++ src/agentgui/core/update/AgentGuiUpdaterTest.java
@@ -6,7 +6,8 @@ public class AgentGuiUpdaterTest {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		new AgentGuiUpdater(true, "http://preupdate.agentgui.org/?key=xml").start();
+//		new AgentGuiUpdater(true, "http://preupdate.agentgui.org/?key=xml").start();
+		new AgentGuiUpdater().start();
 	}
 
 }
