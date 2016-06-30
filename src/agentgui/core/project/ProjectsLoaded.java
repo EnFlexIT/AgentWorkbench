3fc44adf30556a03e71042b87012684201ed6895
Christian Derksen <christian.derksen@uni-due.de>
2016-06-30 09:21:51 +0000

    - Some changes regarding the initialisation of the TimeModelController - New Agent.GUI build
    git-svn-id: svn://project.agent-hygrid.net/agentgui/trunk/AgentGUI@2177 014e83ad-c670-0410-b1a0-9290c87bb784

--- src/agentgui/core/project/ProjectsLoaded.java
+++ src/agentgui/core/project/ProjectsLoaded.java
@@ -183,11 +183,11 @@ public class ProjectsLoaded {
 			newProject.getSimulationSetups().setupCreateDefault();			
 		}
 
-		// --- Load configured PlugIns ----------------------------------------
-		newProject.plugInVectorLoad();
-
 		// --- Load the TimeModelController -----------------------------------
 		newProject.getTimeModelController();
+		
+		// --- Load configured PlugIns ----------------------------------------
+		newProject.plugInVectorLoad();
 
 		// --- Load the environment controller --------------------------------
 		newProject.getEnvironmentController();
