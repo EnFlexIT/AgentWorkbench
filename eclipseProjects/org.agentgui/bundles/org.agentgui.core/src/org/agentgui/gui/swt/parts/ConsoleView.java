7a439c6c425dff532e37beb9d1197f5d75ee57d2
CDerksen <christian.derksen@uni-due.de>
2017-10-27 11:59:55 +0200

    Several changes regarding fx-bridge project configuration and UML
    - Managed to use the SWT/fx-bridge (FxCanvas) in the IDE environment =>
    does currently not work for the tycho build
    - Cleaned up project and classpath settings from Java 7 references
    - Started adjusting class references in ObjectAID class diagrams

--- eclipseProjects/org.agentgui/bundles/org.agentgui.core/src/org/agentgui/gui/swt/parts/ConsoleView.java
+++ eclipseProjects/org.agentgui/bundles/org.agentgui.core/src/org/agentgui/gui/swt/parts/ConsoleView.java
@@ -4,17 +4,22 @@ import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 
 import org.eclipse.e4.ui.di.Focus;
+import org.eclipse.jface.layout.GridDataFactory;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.SWT;
-//import javafx.embed.swt.FXCanvas;
-//import org.eclipse.swt.layout.GridData;
+import javafx.embed.swt.FXCanvas;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.paint.Color;
+
+import org.eclipse.swt.layout.GridData;
 
 public class ConsoleView {
 	
 	private Label lblConsoleView;
-//	private FXCanvas fxCanvas;
+	private FXCanvas fxCanvas;
 
 	public ConsoleView() {
 	}
@@ -24,14 +29,39 @@ public class ConsoleView {
 	 */
 	@PostConstruct
 	public void createControls(Composite parent) {
+		
 		parent.setLayout(new GridLayout(1, false));
 		
 		this.lblConsoleView = new Label(parent, SWT.NONE);
 		this.lblConsoleView.setText("Console View");
-//		
-//		this.fxCanvas = new FXCanvas(parent, SWT.NONE);
-//		this.fxCanvas.setLayoutData(new GridData(SWT.LEFT, SWT.CENTER, true, true, 1, 1));
-//		this.fxCanvas.setLayout(new GridLayout(1, false));
+		
+		
+		this.fxCanvas = new FXCanvas(parent, SWT.NONE);
+		this.fxCanvas.setLayoutData(new GridData(SWT.LEFT, SWT.CENTER, true, true, 1, 1));
+		this.fxCanvas.setLayout(new GridLayout(1, false));
+		
+		
+		GridDataFactory.fillDefaults().grab(true, true).span(3, 1).applyTo(this.fxCanvas);
+		 
+		// create the root layout pane
+		BorderPane layout = new BorderPane();
+		 
+		// create a Scene instance
+		// set the layout container as root
+		// set the background fill to the background color of the shell
+		Scene scene = new Scene(layout, Color.rgb(
+		    parent.getShell().getBackground().getRed(),
+		    parent.getShell().getBackground().getGreen(),
+		    parent.getShell().getBackground().getBlue()));
+		 
+		
+		javafx.scene.control.Label output = new javafx.scene.control.Label();
+		output.setText("Ich bin ein fx-Element");
+		layout.setCenter(output);
+		
+		// set the Scene to the FXCanvas
+		this.fxCanvas.setScene(scene);
+		
 	}
 
 	@PreDestroy
