8fca932474b9960768ba343f02bf5a3e44225605
CDerksen <christian.derksen@uni-due.de>
2022-05-22 23:52:49 +0200

    Added some simple security features to the server configuration:
    - Added SingleUserSecurityHandler
    - Added HTTP to HTTPS redirection option

--- eclipseProjects/org.agentgui/bundles/de.enflexit.awb.ws.core/src/de/enflexit/awb/ws/core/JettyConstants.java
+++ eclipseProjects/org.agentgui/bundles/de.enflexit.awb.ws.core/src/de/enflexit/awb/ws/core/JettyConstants.java
@@ -12,25 +12,25 @@ public enum JettyConstants {
 	HTTP_ENABLED("http.enabled", 1, Boolean.class, true, new Boolean[] {true, false}),
 	HTTP_HOST("http.host", 2, String.class, "0.0.0.0", null),
 	HTTP_PORT("http.port", 3, Integer.class, 8080, null),
-	HTTP_NIO("http.nio", 4, Boolean.class, true, new Boolean[] {true, false}),
+	HTTP_TO_HTTPS("http.to.https", 4, Boolean.class, false, new Boolean[] {true, false}),
 	
-	HTTPS_ENABLED("https.enabled", 5, Boolean.class, false, new Boolean[] {true, false}),
-	HTTPS_HOST("https.host", 6, String.class, "0.0.0.0", null),
-	HTTPS_PORT("https.port", 7, Integer.class, 8443, null),
+	HTTPS_ENABLED("https.enabled", 10, Boolean.class, false, new Boolean[] {true, false}),
+	HTTPS_HOST("https.host", 11, String.class, "0.0.0.0", null),
+	HTTPS_PORT("https.port", 12, Integer.class, 8443, null),
 	
-	SSL_KEYSTORE("ssl.keystore", 8, String.class, null, null),
-	SSL_KEYSTORETYPE("ssl.keystoretype", 9, String.class, KeyStoreType.PKCS12.getType(), KeyStoreType.getAllFileExtensions()),
-	SSL_PASSWORD("ssl.password", 10, String.class, null, null),
-	SSL_KEYPASSWORD("ssl.keypassword", 11, String.class, null, null),
+	SSL_KEYSTORE("ssl.keystore", 13, String.class, null, null),
+	SSL_KEYSTORETYPE("ssl.keystoretype", 14, String.class, KeyStoreType.PKCS12.getType(), KeyStoreType.getAllFileExtensions()),
+	SSL_PASSWORD("ssl.password", 15, String.class, null, null),
+	SSL_KEYPASSWORD("ssl.keypassword", 16, String.class, null, null),
 	
-	SSL_PROTOCOL("ssl.protocol", 12, String.class, "TLS", null),
-//	SSL_ALGORITHM("ssl.algorithm", 13, String.class, null, null),
+	SSL_PROTOCOL("ssl.protocol", 17, String.class, "TLS", null),
+//	SSL_ALGORITHM("ssl.algorithm", 12, String.class, null, null),
 
-	SSL_NEEDCLIENTAUTH("ssl.needclientauth", 14, Boolean.class, false, new Boolean[] {true, false}),
-	SSL_WANTCLIENTAUTH("ssl.wantclientauth", 15, Boolean.class, false, new Boolean[] {true, false}),
+	SSL_NEEDCLIENTAUTH("ssl.needclientauth", 18, Boolean.class, false, new Boolean[] {true, false}),
+	SSL_WANTCLIENTAUTH("ssl.wantclientauth", 19, Boolean.class, false, new Boolean[] {true, false}),
 
-	HTTP_MINTHREADS("http.minThreads", 16, Integer.class, 8  , null),
-	HTTP_MAXTHREADS("http.maxThreads", 17, Integer.class, 200, null);
+	HTTP_MINTHREADS("http.minThreads", 20, Integer.class, 8  , null),
+	HTTP_MAXTHREADS("http.maxThreads", 21, Integer.class, 200, null);
 	
 //	CONTEXT_PATH("context.path", 18, String.class, null, null),
 //	CONTEXT_SESSIONINACTIVEINTERVAL("context.sessioninactiveinterval", 19, Integer.class, 300, null),
---
+++ eclipseProjects/org.agentgui/bundles/de.enflexit.awb.ws.core/src/de/enflexit/awb/ws/core/security/SingleUserSecurityHandler.java
@@ -0,0 +1,90 @@
+package de.enflexit.awb.ws.core.security;
+
+import java.io.IOException;
+
+import org.apache.commons.lang3.StringUtils;
+import org.eclipse.jetty.security.ConstraintSecurityHandler;
+import org.eclipse.jetty.security.DefaultIdentityService;
+import org.eclipse.jetty.security.RoleInfo;
+import org.eclipse.jetty.security.authentication.BasicAuthenticator;
+import org.eclipse.jetty.server.Request;
+import org.eclipse.jetty.server.Response;
+import org.eclipse.jetty.server.UserIdentity;
+
+/**
+ * The Class SingleUserSecurityHandler ... TODO
+ *
+ * @author Christian Derksen - SOFTEC - ICB - University of Duisburg-Essen
+ */
+public class SingleUserSecurityHandler extends ConstraintSecurityHandler {
+
+	private final String login;
+	private final String password;
+	
+	
+	/**
+	 * Instantiates a new SingleUserSecurityHandler that enables an unprotected access
+	 * to the underlying resources (no login and no password is required).
+	 */
+	public SingleUserSecurityHandler() {
+		this(null, null);
+	}
+	/**
+	 * Instantiates a new SingleUserSecurityHandler where access is granted, if the login
+	 * information matching the specified login and password.
+	 *
+	 * @param login the login
+	 * @param password the password
+	 */
+	public SingleUserSecurityHandler(final String login, String password) {
+		this.login = login;
+		this.password = password;
+		
+		this.setAuthenticator(new BasicAuthenticator());
+		this.setIdentityService(new DefaultIdentityService());
+		this.setLoginService(new SingleUserLoginService(login, password));
+	}
+
+	/**
+	 * Checks if is secured.
+	 * @return true, if is secured
+	 */
+	private boolean isSecured() {
+		boolean isSecuredLogin    = this.login!=null && this.login.isBlank()==false;
+		boolean isSecuredPassword = this.password!=null && this.password.isBlank()==false;
+		return isSecuredLogin & isSecuredPassword;
+	}
+	
+	/* (non-Javadoc)
+	 * @see org.eclipse.jetty.security.SecurityHandler#checkUserDataPermissions(java.lang.String, org.eclipse.jetty.server.Request, org.eclipse.jetty.server.Response, org.eclipse.jetty.security.RoleInfo)
+	 */
+	@Override
+	protected boolean checkUserDataPermissions(String pathInContext, Request request, Response response, RoleInfo constraintInfo) throws IOException {
+		return this.isSecured();
+	}
+	
+	/* (non-Javadoc)
+	 * @see org.eclipse.jetty.security.SecurityHandler#isAuthMandatory(org.eclipse.jetty.server.Request, org.eclipse.jetty.server.Response, java.lang.Object)
+	 */
+	@Override
+	protected boolean isAuthMandatory(Request baseRequest, Response base_response, Object constraintInfo) {
+		return this.isSecured();
+	}
+
+	/* (non-Javadoc)
+	 * @see org.eclipse.jetty.security.SecurityHandler#prepareConstraintInfo(java.lang.String, org.eclipse.jetty.server.Request)
+	 */
+	@Override
+	protected RoleInfo prepareConstraintInfo(String pathInContext, Request request) {
+		return null;
+	}
+	
+	/* (non-Javadoc)
+	 * @see org.eclipse.jetty.security.SecurityHandler#checkWebResourcePermissions(java.lang.String, org.eclipse.jetty.server.Request, org.eclipse.jetty.server.Response, java.lang.Object, org.eclipse.jetty.server.UserIdentity)
+	 */
+	@Override
+	protected boolean checkWebResourcePermissions(String pathInContext, Request request, Response response, Object constraintInfo, UserIdentity userIdentity) throws IOException {
+		return userIdentity!=null && StringUtils.equals(login, userIdentity.getUserPrincipal().getName());
+	}
+
+}
