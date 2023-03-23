fa53f1dfffa50b7e439f94ce8ca287b9d1c70ef0
CDerksen <christian.derksen@uni-due.de>
2023-03-23 18:10:54 +0100

    Enabled the SetupPropertiesPanel to react on setup loading events

--- eclipseProjects/de.enflexit.awb/bundles/org.agentgui.core/src/agentgui/core/gui/projectwindow/simsetup/SetupPropertiesPanel.java
+++ eclipseProjects/de.enflexit.awb/bundles/org.agentgui.core/src/agentgui/core/gui/projectwindow/simsetup/SetupPropertiesPanel.java
@@ -1,6 +1,9 @@
 package agentgui.core.gui.projectwindow.simsetup;
 
 import agentgui.core.project.Project;
+import agentgui.core.project.setup.SimulationSetupNotification;
+import de.enflexit.common.Observable;
+import de.enflexit.common.Observer;
 import de.enflexit.common.properties.PropertiesPanel;
 
 /**
@@ -8,10 +11,12 @@ import de.enflexit.common.properties.PropertiesPanel;
  *
  * @author Christian Derksen - SOFTEC - ICB - University of Duisburg-Essen
  */
-public class SetupPropertiesPanel extends PropertiesPanel {
+public class SetupPropertiesPanel extends PropertiesPanel implements Observer {
 
 	private static final long serialVersionUID = 4372968290090987468L;
 
+	private Project currProject;
+	
 	/**
 	 * Instantiates a new project properties panel.
 	 *
@@ -20,7 +25,29 @@ public class SetupPropertiesPanel extends PropertiesPanel {
 	 */
 	public SetupPropertiesPanel(Project currProject, String header) {
 		super(currProject.getSimulationSetups().getCurrSimSetup().getProperties(), header);
+		this.currProject = currProject;
+		this.currProject.addObserver(this);
 	}
-	
+
+	/* (non-Javadoc)
+	 * @see de.enflexit.common.Observer#update(de.enflexit.common.Observable, java.lang.Object)
+	 */
+	@Override
+	public void update(Observable observable, Object updateObject) {
+
+		if (updateObject instanceof SimulationSetupNotification) {
+			// --- Change inside the simulation setup ---------------
+			SimulationSetupNotification scn = (SimulationSetupNotification) updateObject;
+			switch (scn.getUpdateReason()) {
+			case SIMULATION_SETUP_ADD_NEW:
+			case SIMULATION_SETUP_COPY:
+			case SIMULATION_SETUP_LOAD:
+				this.setProperties(this.currProject.getSimulationSetups().getCurrSimSetup().getProperties());
+				break;
+			default:
+				break;
+			}
+		} 
+	}
 	
 }
