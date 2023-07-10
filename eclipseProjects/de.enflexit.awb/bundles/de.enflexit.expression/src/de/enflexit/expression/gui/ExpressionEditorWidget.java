86e2c0acbd44403a2ef709c1f39160d97091e66b
Nils Loose <nils.loose@uni-due.de>
2023-07-10 17:05:53 +0200

    Fixed merge problems

--- eclipseProjects/de.enflexit.awb/bundles/de.enflexit.expression/src/de/enflexit/expression/gui/ExpressionEditorWidget.java
+++ eclipseProjects/de.enflexit.awb/bundles/de.enflexit.expression/src/de/enflexit/expression/gui/ExpressionEditorWidget.java
@@ -5,6 +5,7 @@ import java.awt.Font;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
+import java.awt.Window;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.FocusAdapter;
