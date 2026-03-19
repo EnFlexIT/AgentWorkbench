014fc5295c0c58a21205b4b6da437bdcb6c0751b
CDerksen <christian.derksen@uni-due.de>
2026-03-19 13:04:55 +0100

    Started implementation of data frame core bundle

---
+++ eclipseProjects/de.enflexit.awb/bundles/core/de.enflexit.common/src/de/enflexit/common/dataSources/AbstractDataSource.java
@@ -0,0 +1,84 @@
+package de.enflexit.common.dataSources;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.DiscriminatorColumn;
+import jakarta.persistence.DiscriminatorType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Inheritance;
+import jakarta.persistence.InheritanceType;
+import jakarta.persistence.Table;
+
+/**
+ * The Class AbstractDataSource.
+ *
+ * @author Christian Derksen - SOFTEC - ICB - University of Duisburg-Essen
+ * @author Nils Loose - SOFTEC - ICB - University of Duisburg-Essen
+ */
+@Entity
+@Table(name = "data_source")
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "source_type", discriminatorType = DiscriminatorType.STRING)
+public abstract class AbstractDataSource implements Serializable {
+
+	private static final long serialVersionUID = -1124708006041779794L;
+
+	@Id
+	@GeneratedValue
+	@Column(name="id_data_source", nullable=false)
+	private Integer id;
+	
+	@Column(nullable=false)
+	private String name;
+	private String description;
+	
+	
+	/**
+	 * Returns the id.
+	 * @return the id
+	 */
+	public Integer getId() {
+		return id;
+	}
+	/**
+	 * Sets the id.
+	 * @param id the id to set
+	 */
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	
+	/**
+	 * Gets the name.
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+	/**
+	 * Sets the name.
+	 * @param name the new name
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	/**
+	 * Returns the description.
+	 * @return the description
+	 */
+	public String getDescription() {
+		return description;
+	}
+	/**
+	 * Sets the description.
+	 * @param description the new description
+	 */
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+}
