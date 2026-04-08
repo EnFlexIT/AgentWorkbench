fbe15273945b6ec911b8469b677f2aebb4a5c778
Daniel Bormann <dborman@gmx.de>
2026-04-08 17:48:10 +0200

    Introduced two ApplicationPropertyBus services.
    The performative "DB.CONN.FACTORY.GET" is covered by the new class
    PropertyBusServiceDatabaseConnectionFactoryGet, allowing to read the
    current individual hibernate factory settings.
    To set new settings, there is the new class
    PropertyBusServiceDatabaseConnectionFactorySet with the performative
    "DB.CONN.FACTORY.SET".

---
+++ eclipseProjects/de.enflexit.awb/bundles/database/de.enflexit.db.hibernate/src/de/enflexit/db/hibernate/properties/PropertyBusServiceDatabaseConnectionFactorySet.java
@@ -0,0 +1,123 @@
+package de.enflexit.db.hibernate.properties;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import de.enflexit.common.SecurityPolicies;
+import de.enflexit.common.properties.Properties;
+import de.enflexit.common.properties.PropertyMessage;
+import de.enflexit.common.properties.bus.PropertyBusService;
+import de.enflexit.db.hibernate.HibernateDatabaseService;
+import de.enflexit.db.hibernate.HibernateUtilities;
+import de.enflexit.db.hibernate.connection.DatabaseConnectionManager;
+
+/**
+ * The Class PropertyBusServiceDatabaseConnectionFactorySet. 
+ *
+ * @author Daniel Bormann - EnFlex.IT GmbH
+ */
+public class PropertyBusServiceDatabaseConnectionFactorySet implements PropertyBusService {
+	
+	public static String FACTORYID = "factoryID";
+	public static String DBSYSTEM = "dbSystem";
+
+	/* (non-Javadoc)
+	* @see de.enflexit.common.properties.bus.PropertyBusService#getPerformative()
+	*/
+	@Override
+	public String getPerformative() {
+
+		return "DB.CONN.FACTORY.SET";
+	}
+
+	/* (non-Javadoc)
+	* @see de.enflexit.common.properties.bus.PropertyBusService#setProperties(de.enflexit.common.properties.Properties)
+	*/
+	@Override
+	public boolean setProperties(Properties properties) {
+		
+		// --- If properties aren't valid, don't apply them and return false --------------------------------
+		if (this.hasValidProperties(properties)== false) return false;
+
+		// --- Get the new property values ------------------------------------------------------------------
+		String factory = properties.getStringValue(FACTORYID);
+		String dbSystem = properties.getStringValue(DBSYSTEM);
+		String driver = HibernateUtilities.getDatabaseService(dbSystem).getDriverClassName();
+		String url = properties.getStringValue(HibernateDatabaseService.HIBERNATE_PROPERTY_URL);
+		String catalog = properties.getStringValue(HibernateDatabaseService.HIBERNATE_PROPERTY_Catalog);
+		String user = properties.getStringValue(HibernateDatabaseService.HIBERNATE_PROPERTY_UserName);
+		String password = properties.getStringValue(HibernateDatabaseService.HIBERNATE_PROPERTY_Password);
+		
+		// --- Prepare the hibernate database settings to be saved ------------------------------------------
+		java.util.Properties hibernateDbs = new java.util.Properties();
+		hibernateDbs.put(HibernateDatabaseService.HIBERNATE_PROPERTY_DriverClass, driver);
+		hibernateDbs.put(HibernateDatabaseService.HIBERNATE_PROPERTY_URL, url);
+		hibernateDbs.put(HibernateDatabaseService.HIBERNATE_PROPERTY_Catalog, catalog);
+		hibernateDbs.put(HibernateDatabaseService.HIBERNATE_PROPERTY_UserName, user);
+		hibernateDbs.put(HibernateDatabaseService.HIBERNATE_PROPERTY_Password, password);
+
+		// --- Save new settings and return the result ------------------------------------------------------
+		return DatabaseConnectionManager.getInstance().saveDatabaseConfigurationProperties(factory, hibernateDbs);
+	}
+
+	/**
+	 * Checks for valid properties.
+	 *
+	 * @param properties2check the properties 2 check
+	 * @return true, if successful
+	 */
+	private boolean hasValidProperties(Properties properties2check) {
+
+		List<String> invalidValues = new ArrayList<>();
+		
+		// --- Check the property values and add them to invalidValues if invalid ---------------------------
+		String factory = properties2check.getStringValue(FACTORYID);
+		if (factory == null || factory.isBlank() == true) {
+			invalidValues.add("Factory id is missing");
+		}
+		
+		String dbSystem = properties2check.getStringValue(DBSYSTEM);
+		if (dbSystem == null || dbSystem.isBlank() == true) {
+			invalidValues.add("Database System is missing");
+		}
+		
+		String url = properties2check.getStringValue(HibernateDatabaseService.HIBERNATE_PROPERTY_URL);
+		if (url == null || url.isBlank() == true) {
+			invalidValues.add("URL is missing");
+		}
+		
+		String catalog = properties2check.getStringValue(HibernateDatabaseService.HIBERNATE_PROPERTY_Catalog);
+		if (catalog == null || catalog.isBlank() == true) {
+			invalidValues.add("Catalog is missing");
+		}
+		
+		String user = properties2check.getStringValue(HibernateDatabaseService.HIBERNATE_PROPERTY_UserName);
+		String errorUser = SecurityPolicies.getUserNameError(user);
+		if (errorUser != null) {
+			invalidValues.add(errorUser);
+		}
+
+		String password = properties2check.getStringValue(HibernateDatabaseService.HIBERNATE_PROPERTY_Password);
+		String errorPswd = SecurityPolicies.getPasswordError(password);
+		if (errorPswd != null) {
+			invalidValues.add(errorPswd);
+		}
+		
+		// --- If invalid values were found, add an error message to the properties -------------------------
+		if (invalidValues.size() > 0) {
+			properties2check.setPropertyMessage(PropertyMessage.MessageType.Error, String.join(", ", invalidValues));
+			return false;
+		}
+		return true;
+	}
+
+	/* (non-Javadoc)
+	* @see de.enflexit.common.properties.bus.PropertyBusService#getProperties(de.enflexit.common.properties.Properties)
+	*/
+	@Override
+	public Properties getProperties(Properties properties) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
