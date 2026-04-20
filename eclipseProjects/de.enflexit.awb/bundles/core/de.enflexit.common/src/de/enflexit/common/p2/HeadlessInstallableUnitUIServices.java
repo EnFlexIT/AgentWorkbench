c8f0c876f0d6df9ce38454eb08fc142178691c35
Nils Loose <nils.loose@uni-due.de>
2026-04-20 09:15:15 +0200

    Made the HeadlessInstalableUnitUIService implementation extend a required superclass.

--- eclipseProjects/de.enflexit.awb/bundles/core/de.enflexit.common/src/de/enflexit/common/p2/HeadlessInstallableUnitUIServices.java
+++ eclipseProjects/de.enflexit.awb/bundles/core/de.enflexit.common/src/de/enflexit/common/p2/HeadlessInstallableUnitUIServices.java
@@ -6,6 +6,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
+import org.eclipse.equinox.p2.core.UIServices;
 import org.eclipse.equinox.p2.metadata.IInstallableUnit;
 import org.eclipse.equinox.p2.repository.metadata.spi.IInstallableUnitUIServices;
 
@@ -14,7 +15,7 @@ import org.eclipse.equinox.p2.repository.metadata.spi.IInstallableUnitUIServices
  * accepting a fixed set of artifact repositories without asking the user.  
  * @author Nils Loose - SOFTEC - Paluno - University of Duisburg-Essen
  */
-public class HeadlessInstallableUnitUIServices implements IInstallableUnitUIServices {
+public class HeadlessInstallableUnitUIServices extends UIServices implements IInstallableUnitUIServices {
 	
 	private final Set<URI> trustedRepositories;
 	
@@ -32,6 +33,33 @@ public class HeadlessInstallableUnitUIServices implements IInstallableUnitUIServ
 	@Override
 	public TrustAuthorityInfo getTrustAuthorityInfo(Map<URI, Set<IInstallableUnit>> siteIUs, Map<URI, List<Certificate>> siteCertificates) {
 		return new TrustAuthorityInfo(trustedRepositories, false, false);
+	}
+
+	/* (non-Javadoc)
+	 * @see org.eclipse.equinox.p2.core.UIServices#getUsernamePassword(java.lang.String)
+	 */
+	@Override
+	public AuthenticationInfo getUsernamePassword(String location) {
+		// --- Not required here for now ------------------
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.eclipse.equinox.p2.core.UIServices#getUsernamePassword(java.lang.String, org.eclipse.equinox.p2.core.UIServices.AuthenticationInfo)
+	 */
+	@Override
+	public AuthenticationInfo getUsernamePassword(String location, AuthenticationInfo previousInfo) {
+		// --- Not required here for now ------------------
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.eclipse.equinox.p2.core.UIServices#getTrustInfo(java.security.cert.Certificate[][], java.lang.String[])
+	 */
+	@Override
+	public TrustInfo getTrustInfo(Certificate[][] untrustedChain, String[] unsignedDetail) {
+		// --- Not required here for now ------------------
+		return null;
 	}
 
 }
