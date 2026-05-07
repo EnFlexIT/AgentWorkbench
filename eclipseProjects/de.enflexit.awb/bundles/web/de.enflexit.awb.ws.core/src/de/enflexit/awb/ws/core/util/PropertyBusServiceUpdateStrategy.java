0c7176cece30f162205231f6d53671e9351f8bfd
Daniel Bormann <dnlbormann@gmail.com>
2026-05-07 15:23:31 +0200

    Refactoring of PropertyBusServiceUpdateStrategy.
    The class was moved to ws.core bundle to be able to get and set the
    update strategy for the webApplicationSettings too.

---
+++ eclipseProjects/de.enflexit.awb/bundles/web/de.enflexit.awb.ws.core/src/de/enflexit/awb/ws/core/util/PropertyBusServiceUpdateStrategy.java
@@ -0,0 +1,80 @@
+package de.enflexit.awb.ws.core.util;
+
+import de.enflexit.awb.core.Application;
+import de.enflexit.awb.core.update.AWBUpdater;
+import de.enflexit.awb.ws.core.JettyConfiguration;
+import de.enflexit.awb.ws.core.JettyServerManager;
+import de.enflexit.awb.ws.core.JettyWebApplicationSettings;
+import de.enflexit.awb.ws.core.JettyWebApplicationSettings.UpdateStrategy;
+import de.enflexit.awb.ws.server.AwbServer;
+import de.enflexit.common.properties.Properties;
+import de.enflexit.common.properties.bus.PropertyBusService;
+
+/**
+ * The Class PropertyBusServiceUpdateStrategy.
+ *
+ * @author Daniel Bormann - EnFlex.IT GmbH
+ */
+public class PropertyBusServiceUpdateStrategy implements PropertyBusService {
+
+	public static final String UPDATE_STRATEGY = "update.strategy";
+	public static final String AUTOMATIC = "automatic";
+	public static final String ASKUSER = "askuser";
+
+	/* (non-Javadoc)
+	* @see de.enflexit.common.properties.bus.PropertyBusService#getPerformative()
+	*/
+	@Override
+	public String getPerformative() {
+		return "UPDATE.STRATEGY";
+	}
+
+	/* (non-Javadoc)
+	* @see de.enflexit.common.properties.bus.PropertyBusService#setProperties(de.enflexit.common.properties.Properties, java.lang.String)
+	*/
+	@Override
+	public boolean setProperties(Properties properties, String arguments) {
+		
+		// --- Find out whether the strategy is going to change ---------------------------------------------
+		int currentStrategy = Application.getGlobalInfo().getUpdateAutoConfiguration();
+		int newUpdateStrategy = properties.getStringValue(UPDATE_STRATEGY).equals(AUTOMATIC) ? AWBUpdater.UPDATE_MODE_AUTOMATIC : AWBUpdater.UPDATE_MODE_ASK;
+		
+		if (currentStrategy != newUpdateStrategy) {
+			// --- Get the webAppSettings -------------------------------------------------------------------
+			JettyConfiguration jettyConfig = JettyServerManager.getInstance().getAwbWebRegistry().getRegisteredWebServerService(AwbServer.NAME).getJettyConfiguration();
+			JettyWebApplicationSettings webAppSettings = jettyConfig.getWebApplicationSettings();
+			
+			// --- Set the desired value --------------------------------------------------------------------
+			switch (properties.getStringValue(UPDATE_STRATEGY)) {
+			case "automatic":
+				Application.getGlobalInfo().setUpdateAutoConfiguration(AWBUpdater.UPDATE_MODE_AUTOMATIC);
+				webAppSettings.setUpdateStrategy(UpdateStrategy.Automatic);
+				break;
+			case "askuser":
+				Application.getGlobalInfo().setUpdateAutoConfiguration(AWBUpdater.UPDATE_MODE_ASK);
+				webAppSettings.setUpdateStrategy(UpdateStrategy.AskUser);
+				break;
+			default:
+				return false;
+			}
+			Application.getGlobalInfo().doSaveConfiguration();
+			jettyConfig.save();
+		}
+		
+		return true;
+	}
+
+	/* (non-Javadoc)
+	* @see de.enflexit.common.properties.bus.PropertyBusService#getProperties(de.enflexit.common.properties.Properties, java.lang.String)
+	*/
+	@Override
+	public Properties getProperties(Properties properties, String arguments) {
+		
+		if (properties == null) properties = new Properties();
+		String updateStrategy = Application.getGlobalInfo().getUpdateAutoConfiguration() == AWBUpdater.UPDATE_MODE_AUTOMATIC? AUTOMATIC: ASKUSER;
+		properties.setStringValue(UPDATE_STRATEGY, updateStrategy);
+		
+		return properties;
+	}
+
+}
