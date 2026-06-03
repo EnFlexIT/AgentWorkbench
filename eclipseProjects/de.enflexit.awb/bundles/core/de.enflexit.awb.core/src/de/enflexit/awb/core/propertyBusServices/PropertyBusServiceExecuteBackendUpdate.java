7cb658c3caa16fee1cfc5cdd8bf21fdcc0c6e833
Daniel Bormann <dnlbormann@gmail.com>
2026-06-03 10:29:11 +0200

    Introduced propertyBusService to execute backend updates

---
+++ eclipseProjects/de.enflexit.awb/bundles/core/de.enflexit.awb.core/src/de/enflexit/awb/core/propertyBusServices/PropertyBusServiceExecuteBackendUpdate.java
@@ -0,0 +1,53 @@
+package de.enflexit.awb.core.propertyBusServices;
+
+import org.eclipse.core.runtime.IStatus;
+
+import de.enflexit.awb.core.Application;
+import de.enflexit.common.ExecutionEnvironment;
+import de.enflexit.common.p2.P2OperationsHandler;
+import de.enflexit.common.properties.Properties;
+import de.enflexit.common.properties.bus.PropertyBusService;
+
+/**
+ * The Class PropertyBusServiceExecuteBackendUpdate.
+ *
+ * @author Daniel Bormann - EnFlex.IT GmbH
+ */
+public class PropertyBusServiceExecuteBackendUpdate implements PropertyBusService{
+	
+	public static final String STATUS = "update.status";
+	public static final String RESTARTREQUIRED = "isrestartrequired";
+
+	private Thread workerThread;
+	private IStatus result;
+	
+	/* (non-Javadoc)
+	* @see de.enflexit.common.properties.bus.PropertyBusService#getPerformative()
+	*/
+	@Override
+	public String getPerformative() {
+		return "UPDATE.BACKEND.EXECUTE";
+	}
+
+	/* (non-Javadoc)
+	* @see de.enflexit.common.properties.bus.PropertyBusService#setProperties(de.enflexit.common.properties.Properties, java.lang.String)
+	*/
+	@Override
+	public boolean setProperties(Properties properties, String arguments) {
+		return false;
+	}
+
+	/* (non-Javadoc)
+	* @see de.enflexit.common.properties.bus.PropertyBusService#getProperties(de.enflexit.common.properties.Properties, java.lang.String)
+	*/
+	@Override
+	public Properties getProperties(Properties properties, String arguments) {
+		
+		if (properties == null) properties = new Properties();
+		if (Application.getGlobalInfo().getExecutionEnvironment() == ExecutionEnvironment.ExecutedOverIDE) {
+			return properties;
+		}
+		return properties;
+	}
+
+}
