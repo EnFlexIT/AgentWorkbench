579a804636aaecbb7776810283e20d26567e9d05
Daniel Bormann <dnlbormann@gmail.com>
2026-06-09 11:43:50 +0200

    Finished PropertyBusServiceUpdateWebApp.

--- eclipseProjects/de.enflexit.awb/bundles/web/de.enflexit.awb.ws.core/src/de/enflexit/awb/ws/core/propertyBusServices/PropertyBusServiceUpdateWebApp.java
+++ eclipseProjects/de.enflexit.awb/bundles/web/de.enflexit.awb.ws.core/src/de/enflexit/awb/ws/core/propertyBusServices/PropertyBusServiceUpdateWebApp.java
@@ -5,6 +5,7 @@ import de.enflexit.awb.ws.core.JettyServerManager;
 import de.enflexit.awb.ws.core.JettyWebApplicationSettings;
 import de.enflexit.awb.ws.core.util.WebApplicationUpdate;
 import de.enflexit.awb.ws.core.util.WebApplicationUpdateProcess;
+import de.enflexit.awb.ws.core.util.WebApplicationUpdateProcessListener;
 import de.enflexit.awb.ws.core.util.WebApplicationVersion;
 import de.enflexit.awb.ws.server.AwbServer;
 import de.enflexit.common.properties.Properties;
@@ -17,11 +18,14 @@ import de.enflexit.common.properties.bus.PropertyBusService;
  *
  * @author Daniel Bormann - EnFlex.IT GmbH
  */
-public class PropertyBusServiceUpdateWebApp implements PropertyBusService {
+public class PropertyBusServiceUpdateWebApp implements PropertyBusService, WebApplicationUpdateProcessListener {
 	
 	public static final String STATUS = "update.status";
+	public static final String MESSAGE = "update.message";
 	
-	private WebApplicationUpdateProcess updateProcess;
+	private boolean isUpdateDone;
+	private boolean hasInstalledUpdate;
+	private Thread workerThread;
 	
 	/* (non-Javadoc)
 	* @see de.enflexit.common.properties.bus.PropertyBusService#getPerformative()
@@ -46,22 +50,65 @@ public class PropertyBusServiceUpdateWebApp implements PropertyBusService {
 	public Properties getProperties(Properties properties, String arguments) {
 
 		if (properties == null) properties = new Properties();
+		
+		// --- Start the update thread if not already started -------------------------------------
+		if (workerThread == null) {
+			workerThread = new Thread(this::executeUpdate, "Web Application Update Thread");
+			workerThread.start();
+			properties.setStringValue(STATUS, "Pending");
+			properties.setStringValue(MESSAGE, "");
+			return properties;
+			
+		} else {
+			// --- Update thread already started, evaluating results ------------------------------
+			if (this.isUpdateDone == false) {
+				properties.setStringValue(STATUS, "Pending");
+				properties.setStringValue(MESSAGE, "");
+				
+			} else {
+				
+				if (this.hasInstalledUpdate == true) {
+					properties.setStringValue(STATUS, "Done");
+					properties.setStringValue(MESSAGE, "Update installed");
+				} else {
+					properties.setStringValue(STATUS, "Error");
+					properties.setStringValue(MESSAGE, "Nothing to update");
+				}
+				// --- Reset everything for next call ---------------------------------------------
+				workerThread = null;
+				isUpdateDone = false;
+				hasInstalledUpdate = false;	
+			}
+		}
+		return properties;
+	}
+	
+	/**
+	 * Execute update.
+	 */
+	private void executeUpdate() {
+		
 		// --- Get the webApplicationSettings ---------------------------------------------------------------
 		JettyConfiguration jettyConfig = JettyServerManager.getInstance().getAwbWebRegistry().getRegisteredWebServerService(AwbServer.NAME).getJettyConfiguration();
 		JettyWebApplicationSettings webAppSettings = jettyConfig.getWebApplicationSettings();
 		// --- Search for the latest version ----------------------------------------------------------------
 		WebApplicationVersion latestVersion = WebApplicationUpdate.getWebApplicationUpdate(webAppSettings.getDownloadURL());
-		
-		if (latestVersion == null) {
-			properties.setStringValue(STATUS, "No update available");
-			return properties;
+		// --- Start the update process if a newer version was found ----------------------------------------
+		if (latestVersion != null) {
+			 new WebApplicationUpdateProcess(latestVersion, this).start();
 		} else {
-			if (updateProcess == null) {
-			updateProcess = new WebApplicationUpdateProcess(latestVersion);
-			updateProcess.start();
-			}
+			this.isUpdateDone = true;
 		}
-		return properties;
+		
+	}
+
+	/* (non-Javadoc)
+	* @see de.enflexit.awb.ws.core.util.WebApplicationUpdateProcessListener#onUpdateProcessFinalized()
+	*/
+	@Override
+	public void onUpdateProcessFinalized() {
+		this.isUpdateDone = true;
+		this.hasInstalledUpdate = true;
 	}
 
 }
