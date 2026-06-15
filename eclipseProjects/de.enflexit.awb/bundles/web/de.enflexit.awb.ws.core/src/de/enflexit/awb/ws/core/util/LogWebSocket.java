61af29a6de9d12e625cb8ff20e778e2c2fa5b81b
Daniel Bormann <dnlbormann@gmail.com>
2026-06-15 16:59:11 +0200

    New class LogWebSocket used to send logging events via websocket.

---
+++ eclipseProjects/de.enflexit.awb/bundles/web/de.enflexit.awb.ws.core/src/de/enflexit/awb/ws/core/util/LogWebSocket.java
@@ -0,0 +1,57 @@
+package de.enflexit.awb.ws.core.util;
+
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+
+import jakarta.websocket.OnClose;
+import jakarta.websocket.OnOpen;
+import jakarta.websocket.Session;
+import jakarta.websocket.server.ServerEndpoint;
+
+/**
+ * The Class LogWebSocket.
+ *
+ * @author Daniel Bormann - EnFlex.IT GmbH
+ */
+@ServerEndpoint(value = "/ws/logs")
+public class LogWebSocket {
+
+	private static final Set<Session> sessions = ConcurrentHashMap.newKeySet();
+	
+	/**
+	 * On open.
+	 *
+	 * @param session the session
+	 */
+	@OnOpen
+	public void onOpen(Session session) {
+		sessions.add(session);
+
+		session.getAsyncRemote().sendText("Hello from server");
+	}
+	
+	/**
+	 * On close.
+	 *
+	 * @param session the session
+	 */
+	@OnClose
+	public void onClose(Session session) {
+		sessions.remove(session);
+		System.out.println("Session removed");
+	}
+	
+	/**
+	 * Broadcast.
+	 *
+	 * @param message the message
+	 */
+	public static void broadcast(String message) {
+	    System.out.println("Broadcast to " + sessions.size() + " sessions");
+		for (Session session : sessions) {
+			if (session != null && session.isOpen()) {
+				session.getAsyncRemote().sendText(message);
+			}
+		}
+	}
+}
